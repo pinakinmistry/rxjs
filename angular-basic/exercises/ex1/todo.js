c36876a69ea8fab288925899380282b5178d98d9
pinakinmistry <pinakin.nmistry@gmail.com>
2015-07-04 10:16:51 +0530

    Final cut

--- angular-basic/exercises/ex1/todo.js
+++ angular-basic/exercises/ex1/todo.js
@@ -1,27 +1,27 @@
 angular.module('todoApp', [])
 	.controller('TodoListController', function ($scope) {
 
-		$scope.todoBlocks = [
+		$scope.todoLists = [
 			{
 				title: 'My superheroic Angular todo list',
 				todos: [
 					{text: 'Learn Angular Bascis', done: true},
-					{text: 'Learn Angular Intermediate'},
-					{text: 'Learn Angular Advanced'}
+					{text: 'Learn Angular Intermediate', done: false},
+					{text: 'Learn Angular Advanced', done: false}
 				]
 			},
 			{
 				title: 'My other non superheroic todo list',
 				todos: [
-					{text: 'Do shopping'},
-					{text: 'Clean car'}
+					{text: 'Do shopping', done: false},
+					{text: 'Clean car', done: false}
 				]
 			}
 		];
 
-		$scope.addTodoItem = function (todoBlock, index) {
-			$scope.todoBlocks[index].todos.push({text: todoBlock.todoText});
-			todoBlock.todoText = '';
+		$scope.addTodoItem = function (todoList, index) {
+			$scope.todoLists[index].todos.push({text: todoList.todoText, done: false});
+			todoList.todoText = '';
 		};
 
 	});
